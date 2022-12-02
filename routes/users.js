6317af2fe1354c9d1a66752be9d5606a22ea9be9
agung <agungarsenius@gmail.com>
2022-12-02 15:32:54 +0700

    first commit

---
+++ routes/users.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const router = express.Router()
+const UsersModel = require('../models/users')
+
+//routing endpoint main user
+router.get('/', async(req,res) => { 
+    const users = await UsersModel.findAll()
+
+    res.status(200).json({
+        data: users, 
+        metadata: "Nyobain user enpoin",
+        
+    })
+})
+
+//Method POST
+router.post('/', async(req,res) => { 
+    const {nip, nama, password} = req.body
+
+    const users = await UsersModel.create({
+        nip, nama, password
+    })
+
+    res.status(200).json({
+        data: users, 
+        metadata: "Agoeng",
+        message: "Sobat Ambyar"
+    })
+})
+
+//Method PUT
+router.put('/', async(req,res) => { 
+    const {nip, nama, password, passwordBaru} = req.body
+
+    const userData = await UsersModel.findOne({
+        where: {nip: nip}
+    })
+
+    console.log(userData)
+
+    if(userData.password === password){
+        const users = await UsersModel.update({
+            nama, password: passwordBaru
+        },{
+            where: {nip: nip}
+        })
+        res.status(200).json({
+            userData
+        })
+    } else{ 
+        res.status(400).json({
+            error: "user data invalid"
+        })
+    }
+})
+
+module.exports = router
